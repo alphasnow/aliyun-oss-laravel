c159c7de32b470909cf80cc475191a6997d91897
weifeng <wind91@foxmail.com>
2022-07-12 11:27:59 +0800

    test: actual use test

--- tests/ActualUseTest.php
+++ tests/ActualUseTest.php
@@ -15,41 +15,44 @@ class ActualUseTest extends TestCase
 
         $result = [];
         $result[] = Storage::disk("oss")->putFile("files", $file);
-        $result[] = Storage::disk("oss")->putFileAs("files", $file, "file.txt");
+        $result[] = Storage::disk("oss")->putFileAs("files", $file, "file1.txt");
 
-        $result[] = Storage::disk("oss")->put("texts/file3.txt", file_get_contents($file));
+        $result[] = Storage::disk("oss")->put("contents/file2.txt", file_get_contents($file));
         $fp = fopen($file, "r");
-        $result[] = Storage::disk("oss")->put("texts/file4.txt", $fp);
+        $result[] = Storage::disk("oss")->put("contents/file3.txt", $fp);
         fclose($fp);
 
-        $result[] = Storage::disk("oss")->prepend("file5.txt", "Prepend Text");
-        $result[] = Storage::disk("oss")->append("file5.txt", "Append Text");
+        $result[] = Storage::disk("oss")->prepend("contents/file4.txt", "Prepend Text");
+        $result[] = Storage::disk("oss")->append("contents/file5.txt", "Append Text");
 
-        $result[] = Storage::disk("oss")->put("file7.txt", "My secret", "private");
-        $result[] = Storage::disk("oss")->put("file8.txt", "Download content", ["headers" => ["Content-Disposition" => "attachment; filename=file.txt"]]);
+        $result[] = Storage::disk("oss")->put("contents/file6.txt", "My secret", "private");
+        $result[] = Storage::disk("oss")->put("contents/file7.txt", "Download content", ["headers" => ["Content-Disposition" => "attachment; filename=file.txt"]]);
 
-        $result[] = Storage::disk("oss")->url("files/file.txt");
-        $result[] = Storage::disk("oss")->temporaryUrl("files/file.txt", \Carbon\Carbon::now()->addMinutes(30));
+        $result[] = Storage::disk("oss")->temporaryUrl("contents/file6.txt", \Carbon\Carbon::now()->addMinutes(30));
+        $result[] = Storage::disk("oss")->url("contents/file7.txt");
 
-        $result[] = Storage::disk("oss")->get("files/file.txt");
+        $result[] = Storage::disk("oss")->getVisibility("contents/file5.txt");
+        $result[] = Storage::disk("oss")->setVisibility("contents/file5.txt", "private");
 
-        $result[] = Storage::disk("oss")->exists("files/file.txt");
-        $result[] = Storage::disk("oss")->size("files/file.txt");
-        $result[] = Storage::disk("oss")->lastModified("files/file.txt");
+        $result[] = Storage::disk("oss")->get("files/file1.txt");
+        $result[] = Storage::disk("oss")->readStream("files/file1.txt");
 
-        $result[] = Storage::disk("oss")->copy("files/file.txt", "files/file_new.txt");
-        $result[] = Storage::disk("oss")->move("files/file_new.txt", "files/file_move.txt");
+        $result[] = Storage::disk("oss")->exists("files/file1.txt");
+        $result[] = Storage::disk("oss")->size("files/file1.txt");
+        $result[] = Storage::disk("oss")->lastModified("files/file1.txt");
 
-        $result[] = Storage::disk("oss")->delete("files/file_move.txt");
+        $result[] = Storage::disk("oss")->copy("files/file1.txt", "files/file2.txt");
+        $result[] = Storage::disk("oss")->move("files/file2.txt", "files/file3.txt");
+        $result[] = Storage::disk("oss")->delete("files/file3.txt");
 
-        $result[] = Storage::disk("oss")->makeDirectory("others/first");
-        $result[] = Storage::disk("oss")->deleteDirectory("others/first");
-
-        $result[] = Storage::disk("oss")->files("files");
-        $result[] = Storage::disk("oss")->allFiles("texts");
+        $result[] = Storage::disk("oss")->files("files/");
+        $result[] = Storage::disk("oss")->put("contents/inside/file.txt", "contents");
+        $result[] = Storage::disk("oss")->allFiles("contents/");
 
         $result[] = Storage::disk("oss")->directories("/");
+        $result[] = Storage::disk("oss")->makeDirectory("files/inside/backup");
         $result[] = Storage::disk("oss")->allDirectories("/");
+        $result[] = Storage::disk("oss")->deleteDirectory("files/inside");
 
         $this->assertFalse(in_array(false, $result));
     }
@@ -60,10 +63,10 @@ class ActualUseTest extends TestCase
     public function macro_append_object()
     {
         $result = [];
-        $result[] = Storage::disk("oss")->appendObject("objects/text.txt", "The first line paragraph.", 0);
-        $result[] = Storage::disk("oss")->appendObject("objects/text.txt", "The second line paragraph.", 25);
-        $result[] = Storage::disk("oss")->appendObject("objects/text.txt", "The last line paragraph.", 51);
-        Storage::disk("oss")->delete("objects/text.txt");
+        $result[] = Storage::disk("oss")->appendObject("contents/file8.txt", "The first line paragraph.", 0);
+        $result[] = Storage::disk("oss")->appendObject("contents/file8.txt", "The second line paragraph.", 25);
+        $result[] = Storage::disk("oss")->appendObject("contents/file8.txt", "The last line paragraph.", 51);
+        Storage::disk("oss")->delete("contents/file8.txt");
 
         $this->assertSame([25,51,75], $result);
     }
@@ -75,10 +78,10 @@ class ActualUseTest extends TestCase
     {
         $file = __DIR__."/stubs/file.txt";
         $result = [];
-        $result[] = Storage::disk("oss")->appendFile("objects/file.txt", $file, 0);
-        $result[] = Storage::disk("oss")->appendFile("objects/file.txt", $file, $result[0]);
-        $result[] = Storage::disk("oss")->appendFile("objects/file.txt", $file, $result[1]);
-        Storage::disk("oss")->delete("objects/file.txt");
+        $result[] = Storage::disk("oss")->appendFile("files/file9.txt", $file, 0);
+        $result[] = Storage::disk("oss")->appendFile("files/file9.txt", $file, $result[0]);
+        $result[] = Storage::disk("oss")->appendFile("files/file9.txt", $file, $result[1]);
+        Storage::disk("oss")->delete("files/file9.txt");
 
         $this->assertSame([7,14,21], $result);
     }
